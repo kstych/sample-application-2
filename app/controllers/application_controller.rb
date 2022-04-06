5a4557d976401fe2191f64444e2a96933179c865
Siddharth Upmanyu <siddharth@kstych.com>
2022-04-06 19:40:48 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,18 @@ class ApplicationController < ActionController::Base
     jobs_count = params[:jobs].to_i
     jobs_count.times{ EchoJob.perform_later }
     res = {
-      "server_response": "added #{jobs_count} jobs"
+      "server_response": "added #{jobs_count} EchoJob jobs"
     }
     render :plain => res.to_json, status:200, content_type: "application/json"
   end
 
+  def enqueue_cpucrasherjobs
+    jobs_count = params[:jobs].to_i
+    jobs_count.times{ CpucrasherJob.perform_later }
+    res = {
+      "server_response": "added #{jobs_count} CpucrasherJob jobs"
+    }
+    render :plain => res.to_json, status:200, content_type: "application/json"
+  end
+  
 end
