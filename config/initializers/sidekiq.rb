d335792b9b737967044e869fd0ebab4341b12506
Siddharth Upmanyu <siddharth@kstych.com>
2022-04-06 19:30:58 +0530

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://ey-utility-redis:6379/0' }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://ey-utility-redis:6379/0' }
+end
