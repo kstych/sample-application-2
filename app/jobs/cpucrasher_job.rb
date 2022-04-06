82795384f0f5b84c13a76e668f8ec18b0b8cfcc6
Siddharth Upmanyu <siddharth@kstych.com>
2022-04-06 19:38:53 +0530

    Create cpucrasher_job.rb

---
+++ app/jobs/cpucrasher_job.rb
@@ -0,0 +1,22 @@
+require "benchmark/memory"
+
+FOO = []
+class CpucrasherJob < ApplicationJob
+    
+    def perform(*args)
+        search_until = 800000
+        (1..search_until).each do |i|
+            if ( (is_prime? i) == true )
+                p "#{i} is Prime number"
+            end
+        end
+    end
+    
+    def is_prime?(num)
+      return false if num <= 1
+      Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+      true
+    end
+
+
+end
